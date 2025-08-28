700329c5e0f999dc6d09233ce2c0a66ad6f30081
MatheusCosta616 <matheus.costa2616@gmail.com>
2025-08-28 12:33:30 -0300

    melhoria da api: adicionando um banco em container

---
+++ src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
@@ -0,0 +1,29 @@
+package br.com.dio.persistence.config;
+
+import lombok.NoArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public final class ConnectionConfig {
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            var url = "jdbc:mysql://localhost:3306/board";
+            var user = "root";
+            var password = "123";
+
+            var connection = DriverManager.getConnection(url, user, password);
+            connection.setAutoCommit(false);
+            return connection;
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver JDBC do MySQL não encontrado", e);
+        }
+    }
+}
